e4dc730d7d37e5d169eb44e527d707a8e5e0d71c
Praman1212 <Pramanghimire25@gmail.com>
2022-05-21 12:33:25 +0545

    update model and controller

---
+++ app/Models/Model/Sclass.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sclass extends Model
+{
+    protected $fillable = [
+        'class_name'
+    ];
+}
